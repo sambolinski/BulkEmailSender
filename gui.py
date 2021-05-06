102b9ae8ce48b9a601e84674de6e44fb84d31312
Sambolinski <sam.hughes@city.ac.uk>
2021-05-06 17:49:44 +0100

    Initial Commit

---
+++ gui.py
@@ -0,0 +1,201 @@
+from tkinter import *
+from tkinter.ttk import Frame, Button, Style, Progressbar
+from tkinter import filedialog
+from tkhtmlview import HTMLLabel
+
+class GUI(Frame):
+
+    def __init__(self, root):
+        super().__init__()
+        self.controller = None
+        self.default_title = "Bulk Email Sender"
+        self.current_selection = None
+        self.details_window = None
+        self.job_completion_window = None
+        self.root = root
+
+    def load(self):
+        try:
+            filename = filedialog.askopenfilename(initialdir = "",
+                                                title = "Select a File",
+                                                filetypes = (("Yaml files",
+                                                                "*.yml*"),
+                                                            ("all files",
+                                                                "*.*")))
+        except:
+            return None
+        self.controller.load_yaml(filename)
+
+    def display_details_window(self):
+        self.details_window = DetailsWindow(self)
+
+    def display_job_completion_widow(self):
+        self.job_completion_window = JobCompletionWindow(self)
+
+    def update_html_rederer(self):
+        if(self.current_selection != None):
+            current_email = self.controller.generated_email_list[self.current_selection]
+            to_list = ", ".join(current_email.to)
+            cc_list = ", ".join(current_email.cc)
+
+            html = "<b>To:</b> " + to_list + "<br>"
+            html +="<b>CC:</b> " + cc_list + "<br>"
+            html +="<b>Subject:</b> " + current_email.subject+"<br><br>"
+            current_email.body_raw = self.html_editor.get("1.0",'end-1c')
+            current_email.parse_body(self.controller.worksheet,self.controller.email_config["DATA"])
+            self.email_generated_display.set_html("<html>"+html+self.controller.generated_email_list[self.current_selection].body_generated+"</html>")
+        else:
+            self.email_generated_display.set_html("<html></html>")
+        self.root.update_idletasks()
+    def listbox_listener(self, event):
+        if(self.email_list_box.curselection()):
+            self.current_selection = int(self.email_list_box.curselection()[0])
+        self.update_html_rederer()
+    def html_editor_listener(self, event):
+        if(self.email_list_box.curselection()):
+            self.current_selection = int(self.email_list_box.curselection()[0])
+        self.update_html_rederer()
+    def initialise(self):
+        self.master.title(self.default_title)
+        self.style = Style()
+        self.style.theme_use("default")
+        #Main Menu
+        
+        self.mainmenu = Menu(self.master)
+        self.mainmenu.add_command(label = "Load", command= self.load)
+        self.mainmenu.add_command(label = "Exit", command= self.master.destroy) 
+        self.mainmenu.add_command(label = "Generate Report", command = self.controller.logging.output_to_file, state='disabled') 
+        self.master.config(menu = self.mainmenu)
+
+        self.frame = Frame(self, relief=RAISED, borderwidth=1)
+        self.frame.pack(fill=BOTH, expand=True)
+
+        self.pack(fill=BOTH, expand=True)
+
+        self.email_list_box = Listbox(self.frame)  
+        self.email_list_box.bind("<<ListboxSelect>>", self.listbox_listener)
+        self.email_list_box.pack(side = LEFT, fill=Y)   
+
+        self.html_editor = HTMLLabel(self.frame,relief=RIDGE, background="white")
+        self.html_editor.bind('<KeyRelease>',  self.html_editor_listener)
+        self.html_editor.pack(fill=BOTH, expand=True)
+        self.html_editor.fit_height()
+
+        self.email_generated_display = HTMLLabel(self.frame, relief=RIDGE, state='disabled')
+        self.email_generated_display.pack(fill=BOTH, expand=True)
+        self.email_generated_display.fit_height()
+
+        self.send_emails_button = Button(self, text="SEND", command=self.controller.init_sending_emails_authentication)
+        self.send_emails_button.pack(side=RIGHT, padx=5, pady=5)
+
+        self.progress_bar = Progressbar(self, mode="determinate", value=0, maximum=100)
+        self.progress_bar.pack(side=LEFT, fill=BOTH, expand=True, padx=5, pady=5)
+    def update_list(self, generated_email_list):
+        self.email_list_box.delete(0, END) 
+        for email in generated_email_list: 
+            if(len(email.to) > 0):
+                self.email_list_box.insert(END, email.to[0]) 
+            else:
+                self.email_list_box.insert(END, "NULL") 
+        if(len(generated_email_list) > 0):
+            self.email_list_box.select_clear(0, END)
+            self.email_list_box.select_set(0)
+            if(self.email_list_box.curselection()):
+                self.current_selection = int(self.email_list_box.curselection()[0])
+                self.update_html_rederer()
+    def update_progress_bar(self, amount):
+        self.progress_bar.step(amount)
+        self.root.update_idletasks()
+    def reset(self):
+        self.current_selection = None
+        self.progress_bar['value'] = 0
+        self.update_html_rederer()
+        self.update_list(self.controller.generated_email_list)
+        self.mainmenu.entryconfig(3, state="disabled" )
+        
+##REFACTOR TO INHERIT FROM Toplevel
+class DetailsWindow:
+    def __init__(self,parent):
+        self.master = parent
+        if self.master != "":
+            self.window = Toplevel(self.master,width=200,height=100,padx=40,pady=40)
+
+        #window settings
+        self.window.title("Login")
+        self.window.protocol("WM_DELETE_WINDOW", self.disable_event)
+        self.window.resizable(False, False)
+
+        #window widgets
+        self.details_canvas = Canvas(self.window, width = 200, height = 100)
+        self.details_canvas.pack()
+
+        self.username_input = Entry(self.window)
+        self.details_canvas.create_window(100, 30, window=self.username_input)
+        self.username_label = Label(self.window, text = "username")
+        self.details_canvas.create_window(10, 30, window=self.username_label)
+
+        self.password_input = Entry (self.window,show="*") 
+        self.details_canvas.create_window(100, 50, window=self.password_input)
+        self.password_label = Label(self.window, text = "password")
+        self.details_canvas.create_window(10, 50, window=self.password_label)
+
+        self.cancel_button = Button(self.window,text="Cancel",command=self.window.destroy)
+        self.details_canvas.create_window(60, 80, window=self.cancel_button)
+
+        self.login_button = Button(self.window,text="Login",command=self.login_button_onclick)
+        self.details_canvas.create_window(135, 80, window=self.login_button)
+        
+        self.authentication_status_text = StringVar()
+        self.authentication_status_label = Label(self.window, textvariable=self.authentication_status_text)
+        self.details_canvas.create_window(100, 105, window=self.authentication_status_label)
+    def login_button_onclick(self):
+        self.master.controller.bulk_send_emails(self.username_input.get(), self.password_input.get())
+    
+    def reset(self):
+        self.authentication_status_text.set("")
+    def disable_event(self):
+        pass
+    def update_authentication_status_label(self, text):
+        self.authentication_status_text.set(text)
+        self.master.root.update_idletasks()
+
+class JobCompletionWindow:    
+    def __init__(self,parent):
+        self.master = parent
+        if self.master != "":
+            self.window = Toplevel(self.master,width=200,height=100,padx=40,pady=40)
+
+        #window settings
+        self.window.title("Job Completed")
+        self.window.resizable(False, False)
+
+        #window widgets
+        self.details_canvas = Canvas(self.window, width = 200, height = 100)
+        self.details_canvas.pack()
+        
+        self.job_completion_status = StringVar()
+        self.job_completion_status_label = Label(self.window, textvariable=self.job_completion_status)
+        self.details_canvas.create_window(100, 10, window=self.job_completion_status_label)
+
+        self.cancel_button = Button(self.window,text="Close",command=self.window.destroy)
+        self.details_canvas.create_window(60, 80, window=self.cancel_button)
+
+        self.login_button = Button(self.window,text="Generate Report",command=self.master.controller.logging.output_to_file)
+        self.details_canvas.create_window(150, 80, window=self.login_button)
+    
+    def update_job_completion_status(self):
+        text = "Emails Sent Success: " + str(len(self.master.controller.logging.emails_success)) + "\n"
+        text += "Emails Sent Failure: " + str(len(self.master.controller.logging.emails_failed))
+        self.job_completion_status.set(text)
+        self.master.root.update_idletasks()
+
+def main():
+
+    root = Tk()
+    root.geometry("960x720+150+150")
+    gui = GUI(root)
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,212 @@
+from tkinter import *
+
+import yaml
+from openpyxl import load_workbook
+
+from gui import GUI, DetailsWindow
+from email.mime.text import MIMEText
+import smtplib
+from datetime import datetime
+
+##########################
+#       EMAIL DATA       #
+##########################
+class GeneratedEmail:
+    static_id = 0
+    def __init__(self, to, cc, subject, body_raw, row):
+        self.to = to
+        self.cc = cc
+        self.subject = subject
+        self.body_raw = body_raw
+        self.body_generated = body_raw
+        self.row = row
+        GeneratedEmail.static_id += 1
+        self.id = GeneratedEmail.static_id
+    def parse_body(self, excel_data, data_config):
+        self.body_generated = self.body_raw
+        for data in data_config:
+            data_converted = str(excel_data.cell(row=self.row, column=list(data.values())[0]).value)
+            self.body_generated = self.body_generated.replace("%"+list(data.keys())[0], data_converted)
+    def generate_mimetext(self, sender):
+        #Email formatting
+        mimetext_formatted = MIMEText(self.body_generated, 'html')
+        mimetext_formatted["From"] = sender
+        mimetext_formatted["Subject"] = self.subject
+        mimetext_formatted["To"] = ", ".join(self.to)
+        mimetext_formatted["CC"] = ", ".join(self.cc)
+        return mimetext_formatted
+    def __str__(self):
+        return "to:  {0}, cc: {1}, subject: {2}".format(self.to, self.cc, self.subject)
+
+
+class Controller:
+    def __init__(self, gui):
+        self.initialise()
+        #gui
+        self.gui = gui
+        self.gui.controller = self
+        self.logging = Logging()
+        
+
+    def initialise(self):
+        #server
+        self.smtp_server = None
+    
+        #authentication status
+        self.authentication_status = ""
+        #Data configs
+        self.email_config = None
+
+        #
+        self.workbook = None
+        self.worksheet = None 
+
+        #email data
+        self.generated_email_list = []
+
+        self.logging = Logging()
+        
+
+    def load_yaml(self, filename):
+        if(filename != None and filename != ""):
+            self.initialise()
+            self.gui.reset()
+            with open(filename,'r') as file:
+                self.email_config = yaml.load(file, Loader=yaml.FullLoader)
+            self.gui.html_editor.delete('1.0', END) 
+            self.gui.html_editor.insert(END,self.email_config["EMAIL_TEMPLATE"])
+            self.load_excel_data()
+            self.generated_email()
+            self.gui.master.title(self.gui.default_title + " - "+str(len(self.generated_email_list)) + " emails loaded")
+    def init_sending_emails_authentication(self):
+        if(len(self.generated_email_list) > 0):
+            self.gui.display_details_window()
+    
+    def calculate_normalised_range(self, range_config):
+        return range(int(range_config["START"]), int(range_config["END"])+1)
+
+    def check_if_null(self, value):
+        return value == None or value == "None" or value == ""
+
+    def bulk_send_emails(self, username, password):
+        if(self.init_server()):
+            if(self.login(username,password)):
+                for email in self.generated_email_list:
+                    email.body_raw = self.gui.html_editor.get("1.0",'end-1c')
+                    email.parse_body(self.worksheet, self.email_config["DATA"])
+                    self.send_email(email, username)
+                #email completion window
+                self.gui.display_job_completion_widow()
+                self.gui.job_completion_window.update_job_completion_status()
+                #can request report
+                self.gui.mainmenu.entryconfig(3, state="normal" )
+
+    def send_email(self, email, username):
+        formatted_email = email.generate_mimetext(username)
+        try:
+            self.smtp_server.sendmail(username, formatted_email["To"]+","+formatted_email["CC"],formatted_email.as_string())
+            if(self.gui.progress_bar['value'] + 100.0/len(self.generated_email_list) >= 100):
+                self.gui.progress_bar['value'] = 100.0
+            else:
+                self.gui.update_progress_bar(100.0/len(self.generated_email_list))
+            self.logging.emails_success.append(email)
+            return True
+        except:
+            self.logging.emails_failed.append(email)
+            return False
+
+    def init_server(self):
+        self.gui.details_window.login_button.config(state='disabled')
+        server = self.email_config["SERVER"]
+        port = int(self.email_config["PORT"])
+        try:
+            self.gui.details_window.update_authentication_status_label("Connecting to " + server + " on port: "+str(port))
+            self.smtp_server = smtplib.SMTP(server, port)
+            self.smtp_server.connect(server, port)
+            self.smtp_server.ehlo()
+            self.smtp_server.starttls()
+            self.smtp_server.ehlo()
+            return True
+        except Exception as e:
+            print(e)
+            self.gui.details_window.update_authentication_status_label("Connection failed")
+            self.gui.details_window.login_button.config(state='normal')
+            return False
+
+    def login(self, username, password):
+        try:
+            self.gui.details_window.update_authentication_status_label("Connection Success. Attempting login")
+            self.smtp_server.login(username, password)
+            self.gui.details_window.window.destroy()
+            return True
+        except Exception as e:
+            print(e)
+            self.gui.details_window.update_authentication_status_label("Login failed")
+            self.reset_server()
+            self.gui.details_window.login_button.config(state='normal')
+            return False
+    
+    def reset_server(self):
+        self.smtp_server.close()
+
+    def get_recipients_from_excel(self, row, recipient):
+        if(not self.check_if_null(self.email_config[recipient[1]])):
+            for recipient_type_list in self.email_config[recipient[1]]:
+                if(not self.check_if_null(list(recipient_type_list.values())[0])):
+                    value_in_cell = str(self.worksheet.cell(row=row, column=list(recipient_type_list.values())[0]).value)
+                    if(not self.check_if_null(value_in_cell)):
+                        recipient[0].append(value_in_cell)
+                        
+    def generated_email(self):
+        #email generation
+        self.generated_email_list = []
+        for row in self.calculate_normalised_range(self.email_config["RANGE"]):
+            #recipients details
+            to = []
+            cc = []
+            recipient_list = [(to, "TO"), (cc, "CC")]
+            for recipient in recipient_list:
+                self.get_recipients_from_excel(row, recipient)
+
+            #email details
+            subject = self.email_config["SUBJECT"]
+            body_template = self.email_config["EMAIL_TEMPLATE"]
+            generated_email = GeneratedEmail(to,cc,subject,body_template, row)
+            self.generated_email_list.append(generated_email)
+        self.gui.update_list(self.generated_email_list)
+
+    def load_excel_data(self):
+        self.workbook = load_workbook(self.email_config["FILE_DIRECTORY"])
+        self.worksheet = self.workbook[self.email_config["SHEET_NAME"]]
+    def initialise_GUI(self):
+        self.gui.initialise()
+
+class Logging:
+    def __init__(self):
+        self.emails_failed = []
+        self.emails_success = []
+    def output_to_file(self):
+        try:
+            file = open("email_report_"+str(datetime.now().strftime("%Y_%m_%d__%H_%M_%S")+".txt"),'x')
+            file.write("emails failed"+"\n")
+            for email in self.emails_failed:
+                file.write(str(email)+"\n")
+            file.write("\n\n\n\n")
+            file.write("emails success"+"\n")
+            for email in self.emails_success:
+                file.write(str(email)+"\n")
+            file.close()
+        except:
+            pass
+
+def main():
+    #GUI    
+    root = Tk()
+    root.geometry("960x720+150+150")
+    gui = GUI(root)
+    controller = Controller(gui)
+    controller.initialise_GUI()
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
